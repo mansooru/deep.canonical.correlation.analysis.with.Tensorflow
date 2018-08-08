ab9eebd1b9baca4f6e39ef99ac18032280453ad2
Mansu Kim <mansooru.kim@gmail.com>
2018-08-09 00:22:01 +0900

    Update dcca.with.mnist.py

--- dcca.with.mnist.py
+++ dcca.with.mnist.py
@@ -27,43 +27,18 @@ test_X2 = data['XTe2']
 testLabel = data['testLabel']
 
 # view1 layers
-X1 = tf.placeholder(tf.float32, [None,784])
-X2 = tf.placeholder(tf.float32, [None,784])
-
-W11 = tf.Variable(tf.random_uniform([784, 1024],-1,1))
-B11 = tf.Variable(tf.random_uniform([784, 1024],-1,1))
-L11 = tf.nn.relu(tf.add(tf.matmul(X1,W11),B11))
-
-W21 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-B21 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-L21 = tf.nn.relu(tf.add(tf.matmul(L11,W21),B21))
-
-W31 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-B31 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-L31 = tf.nn.relu(tf.add(tf.matmul(L21,W31),B31))
-
-W41 = tf.Variable(tf.random_uniform([1024, 10],-1,1))
-B41 = tf.Variable(tf.random_uniform([1024, 10],-1,1))
-model1 = tf.add(tf.matmul(L31,W41),B41)
+h11 = tf.layers.dense(X1, 784,activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
+h21 = tf.layers.dense(h11, 1024,activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
+h31 = tf.layers.dense(h21, 1024,activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
+h41 = tf.layers.dense(h31, 10, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
 
 # view2 layers
-W12 = tf.Variable(tf.random_uniform([784, 1024],-1,1))
-B12 = tf.Variable(tf.random_uniform([784, 1024],-1,1))
-L12 = tf.nn.relu(tf.add(tf.matmul(X2,W12),B12))
+h12 = tf.layers.dense(X2, 784,activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
+h22 = tf.layers.dense(h12, 1024,activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
+h32 = tf.layers.dense(h22, 1024,activation=tf.nn.relu, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
+h42 = tf.layers.dense(h32, 10, kernel_regularizer=tf.contrib.layers.l2_regularizer(scale=0.01))
 
-W22 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-B22 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-L22 = tf.nn.relu(tf.add(tf.matmul(L12,W22),B22))
-
-W32 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-B32 = tf.Variable(tf.random_uniform([1024, 1024],-1,1))
-L32 = tf.nn.relu(tf.add(tf.matmul(L22,W32),B32))
-
-W42 = tf.Variable(tf.random_uniform([1024, 10],-1,1))
-B42 = tf.Variable(tf.random_uniform([1024, 10],-1,1)
-model2 = tf.add(tf.matmul(L32,W42),B42)
-
-cost = neg_correlation(model1,model2,10)
+cost = neg_correlation(h41,h42,10)
 optimizer = tf.train.AdamOptimizer(0.01).minimize(cost)
 
 init = tf.global_variables_initializer()
@@ -88,9 +63,9 @@ for epoch in range(n_epochs):
 
         iterations += 1
 
-X1proj, X2proj = sess.run([model1, model2],feed_dict={X1: train_X1 ,X2: train_X2})
-XV1proj, XV2proj = sess.run([model1, model2],feed_dict={X1: tune_X1,X2: tune_X2})
-XTe1proj, XTe2proj = sess.run([model1, model2],feed_dict={X1: test_X1,X2: test_X2})
+X1proj, X2proj = sess.run([h41,h42],feed_dict={X1: train_X1 ,X2: train_X2})
+XV1proj, XV2proj = sess.run([h41,h42],feed_dict={X1: tune_X1,X2: tune_X2})
+XTe1proj, XTe2proj = sess.run([h41,h42],feed_dict={X1: test_X1,X2: test_X2})
 
 print("Linear CCA started!")
 w = [None, None]
@@ -105,11 +80,6 @@ XV1proj = np.dot(XV1proj, w[0])
 
 XTe1proj -= m[0].reshape([1, -1]).repeat(len(XTe1proj), axis=0)
 XTe1proj = np.dot(XTe1proj, w[0])
-
-trainLable = trainData.labels.astype('float')
-tuneLable = tuneData.labels.astype('float')
-testLable = testData.labels.astype('float')
-
 
 ################# SVM classify #############################
 
