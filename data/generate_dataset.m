3f67e4d289ba3714481a663a307f33daf7f7dae6
Mansu Kim <mansooru.kim@gmail.com>
2018-08-25 19:31:05 +0900

    noise_update

--- data/generate_dataset.m
+++ data/generate_dataset.m
@@ -7,7 +7,7 @@ q = 1024;
 
 max_zero = 150;
 max_block =80;
-u0_gt = sb_vector(p,max_zero,max_block);
+u0_block = sb_vector(p,max_zero,max_block);
 
 if length(u0_gt) > p
     u0_gt(q+1:end)=[];
@@ -21,6 +21,8 @@ if length(v0_gt) > q
     v0_gt(q+1:end)=[];
 end
 
+stem(v0_gt);
+figure; stem(u0_gt);
 
 v0_gt = v0_gt>0;
 u0_gt = u0_gt>0;
@@ -29,10 +31,10 @@ u0_gt = u0_gt>0;
 n = 5000;
 z =  zscore(rand(n,1));
 noise_level = ceil(rand*1);
-input_X1  =z*u0_gt + randn(n,q) .*rand(n,q)*10;
+input_X1  =z*u0_gt + randn(n,q) .*rand(n,q)*4;
 label_X1= u0_gt';
 
-input_X2 = z*v0_gt + randn(n,q).*rand(n,q)*10;
+input_X2 = z*v0_gt + randn(n,q).*rand(n,q)*4;
 label_X2 = v0_gt';
 
 input_X1 = zscore(input_X1);
@@ -47,10 +49,10 @@ clear input_X1 input_X2 label_X1 label_X2 i
 % Generate X and Y
 n = 1000;
 z1 =  zscore(rand(n,1));
-input_X1 = z1*u0_gt + randn(n,q) .*rand(n,q)*10;
+input_X1 = z1*u0_gt + randn(n,q) .*rand(n,q)*3;
 label_X1 = u0_gt';
 
-input_X2 = z1*v0_gt + randn(n,q).*rand(n,q)*10;
+input_X2 = z1*v0_gt + randn(n,q).*rand(n,q)*3;
 label_X2 = v0_gt';
 
 
